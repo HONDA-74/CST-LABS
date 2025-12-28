297bdb48d9b0926283c218883c5aed27ba0b2b8d
Mohanad Tarek <mohanadtarek742203@gmail.com>
2025-12-28 15:41:46 +0200

    Add files via upload

---
+++ Lab3/task3.js
@@ -0,0 +1,6 @@
+
+//task 3
+let num1 = parseFloat(prompt("Enter first number : " , "1"));
+let num2 = parseFloat(prompt("Enter second number : " , "1"));
+
+(num1 > num2) ? console.log("The larger number is : " + num1) : console.log("The larger number is : " + num2);
